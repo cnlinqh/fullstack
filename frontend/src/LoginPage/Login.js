68c4e5b6af2e1039a2b51da8bd1441ba56918a6e
Quan-hao.Lin <quan-hao.lin@sap.com>
2019-09-12 20:32:36 +0800

    update login page

--- frontend/src/LoginPage/Login.js
+++ frontend/src/LoginPage/Login.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 class Login extends Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                name: "",
+                password: ""
+            }
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        var { name, value } = event.target;
+        var user = this.state.user;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        // this.props.register(this.state.user.name, this.state.user.password);
+    }
+
+    render() {
         return (
-            <h1>Login</h1>
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>
+                            Name
+                        </label>
+                        <input type="text" name="name" value={this.state.user.name} onChange={this.handleChange}>
+                        </input>
+                    </div>
+                    <div>
+                        <label>
+                            Password
+                        </label>
+                        <input type="text" name="password" value={this.state.user.password} onChange={this.handleChange}>
+                        </input>
+                    </div>
+                    <div>
+                        <button>
+                            Login
+                        </button>
+                        <Link to="/register">
+                            Register
+                        </Link>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
-
-export { Login };
+function mapStateToProps(state) {
+    // const { status } = state.registration;
+    // return { status }
+}
+const mapDispatchToProps = {
+    // register: registrationActions.register
+}
+const connectedLogin = connect()(Login)
+// export { connectedRegister as Register };
+export { connectedLogin as Login };
