d89b3fc25763b2e416840b5bf43a8cf052143275
Quan-hao.Lin <quan-hao.lin@sap.com>
2019-09-22 16:46:56 +0800

    add navigation
    1, login -> register  // go to Sign Up
    2, register -> login // back to Sign In

--- mobile/lib/pages/login.dart
+++ mobile/lib/pages/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/pages/register.dart';
+
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
 
@@ -8,8 +10,54 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('Login'),
+    final nameField = TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: "Name",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+    );
+
+    final loginButton = RaisedButton(
+      child: Text("Login"),
+      onPressed: () {},
+    );
+
+    final registerButton = FlatButton(
+      child: Text("Sign Up"),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => RegisterPage()),
+        );
+      },
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            nameField,
+            passwordField,
+            loginButton,
+            registerButton,
+          ],
+        ),
+      ),
     );
   }
 }
--- mobile/lib/pages/register.dart
+++ mobile/lib/pages/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class RegisterPage extends StatefulWidget {
   RegisterPage({Key key}) : super(key: key);
 
@@ -8,8 +9,63 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('Register'),
+    final nameField = TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: "Name",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: "Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+    );
+
+    final password2Field = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: "Confirm Password",
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+    );
+
+    final registerButton = RaisedButton(
+      child: Text("Sign In"),
+      onPressed: () {},
+    );
+
+    final cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            nameField,
+            passwordField,
+            password2Field,
+            registerButton,
+            cancelButton,
+          ],
+        ),
+      ),
     );
   }
 }
